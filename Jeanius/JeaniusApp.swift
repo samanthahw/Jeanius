774a32744ad93505b6c5ca18fac6ded97d4aaa4d
Samantha Wong <samanthaw9876@gmail.com>
2024-07-30 09:21:17 -0700

    Initial Commit

---
+++ Jeanius/JeaniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeaniusApp.swift
+//  Jeanius
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct JeaniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
